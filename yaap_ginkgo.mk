81c02852dc7b61ec8961bdca4c6dea1a64e9ed70
mnasibzade <euoryexe@gmail.com>
2024-10-18 01:27:18 -0700

    ginkgo: LineageOS -> YAAP - lineage_ginkgo -> yaap_ginkgo - Some changes on BoardConfig.mk and etc
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- yaap_ginkgo.mk
+++ yaap_ginkgo.mk
@@ -23,11 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common YAAP stuff
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := yaap_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
